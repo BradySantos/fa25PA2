32fbb2726497e0d157d60c27be49c7ece21b003b
Brady Santos <bsantos5238@sdsu.edu>
2025-10-26 22:02:09 -0700

    Pop, push, upheap, downheap updated

--- heap.h
+++ heap.h
@@ -12,43 +12,43 @@ struct MinHeap {
 
     void push(int idx, int weightArr[]) {
         // TODO: insert index at end of heap, restore order using upheap().
-        // Fix this size condition below::::::::::::::::::::::::::::::
-        if (idx < 0 || idx >= size) { // Check index out of bounds
+        // Fix this size condition below:::::::::::::::::::::::::::::: What is max array size?
+        if (size >= ???) { // Check index out of bounds
             cout << "Index out of bounds. Cannot push." << endl;
             return;
         }
 
-        weightArr[size] = weightArr[idx]; // Insert index at end of heap; size value is index at end of heap
-        // data[size] = weightArr[size];, figure out data use, might not need this line
-        size++;
+        weightArr[size] = idx; // Insert index at end of heap
 
-        // upheap(idx, weightArr); // Upheap after insertion
+        upheap(size, weightArr); // Upheap after insertion
 
+        size++; // Update size
     }
 
     int pop(int weightArr[]) {
         // TODO: remove and return smallest index
         // Replace root with last element, then call downheap()
+        int minIndex = weightArr[0]; // Save smallest index
+
         if (size == 0) {  // If heap is already empty, do not pop; just return 0 index
-            cout << "Heap is empty. Cannot pop; returning 0 index." << endl;
-            return 0; // Should 0 be returned??????
+            cout << "Heap is empty. Cannot pop; returning -1 index." << endl;
+            return -1;
         }
         if (size == 1) { // If heap only has a root, remove root and return 0 index
-            cout << "Heap only has one element. Removing and returning 0 index." << endl;
-            // weightArr[size - 1] = -1; Figure out how to set array as "empty"
-            return 0;
+            cout << "Heap only has one element. Removing and returning smallest index." << endl;
+            size--;
+            return minIndex;
         }
 
         weightArr[0] = weightArr[size - 1]; // Replace root with last element
         size--;
 
-// Downheap:::::::
+        downheap(0, weightArr); // Downheap from root after deletion
 
-
-        return weightArr[0]; // Return root (contains smallest index)
+        return minIndex; // Return root (contains smallest index)
     }
 
-    void upheap(int pos, int weightArr[]) {
+    void upheap(int pos, int weightArr[]) { // Upheap and downheap should both be O(logn); therefore also pop and push
         // TODO: swap child upward while smaller than parent
         if (size == 0) { // If empty, cannot upheap
             cout << "Heap is empty. Cannot upheap." << endl;
@@ -60,14 +60,13 @@ struct MinHeap {
         }
 
         int i = pos;
-        while (weightArr[i] < weightArr[(i - 1) / 2]) { // Compare target index to parent
+        while ((i > 0) && (weightArr[i] < weightArr[(i - 1)/ 2])) { // Compare target index to parent
             int tempParent = weightArr[(i - 1) / 2]; // Store old parent
-            weightArr[(i - 1) / 2] = weightArr[i];
+            weightArr[(i - 1) / 2] = weightArr[i]; // Parent: (i - 1) / 2
             weightArr[i] = tempParent;
-        }
-        // Parent: (i - 1) / 2, children: 2*i + 1, 2*i + 2.
-        // Fix i?
 
+            i = (i - 1) / 2; // Move up (O(logn))
+        }
     }
 
     void downheap(int pos, int weightArr[]) {
@@ -81,21 +80,28 @@ struct MinHeap {
             return;
         }
 
-        int i = pos;
-        while ((weightArr[(i - 1) / 2] > weightArr[2*i + 1]) ||
-              (weightArr[(i - 1) / 2] > weightArr[2*i + 2])) { // Downheap if parent is greater than either child
-            if (weightArr[(i - 1) / 2] > weightArr[2*i + 1]) { // If parent is greater than left child, swap
-                int tempParent = weightArr[(i - 1) / 2];
-                weightArr[(i - 1) / 2] = weightArr[2*i + 1];
-                weightArr[2*i + 1] = tempParent;
-            }
-            if (weightArr[(i - 1) / 2] > weightArr[2*i + 2]) { // If parent is greater than right child, swap
-                int tempParent = weightArr[(i - 1) / 2];
-                weightArr[(i - 1) / 2] = weightArr[2*i + 2];
-                weightArr[2*i + 2] = tempParent;
+        int i = pos; // Parent start
+        int currChild; // Track which child is being traced
+
+        while (2 * i + 1 < size) { // Enter loop if left child is within array size
+            int leftChild = 2 * i + 1; // Children: 2*i + 1, 2*i + 2.
+            int rightChild = 2 * i + 2;
+
+            currChild = leftChild; // Start at left child
+
+            if ((rightChild < size) && (weightArr[rightChild] < weightArr[currChild])) {
+                currChild = rightChild; // If right child is in array and less than left, set right as current
             }
 
-            // Fix??
+            if (weightArr[currChild] >= weightArr[i]) { // If smallest child is already smaller than parent, no need for downheap
+                break;
+            }
+
+            int tempParent = weightArr[i]; // Swap parent down
+            weightArr[i] = weightArr[currChild];
+            weightArr[currChild] = tempParent;
+
+            i = currChild; // Update parent to child (O(logn))
         }
     }
 };
